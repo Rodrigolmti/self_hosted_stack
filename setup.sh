cd47d69d7e951c4a6005e0db90e88f3e4a0da68f
Rodrigo Lopes <rodrigolmti@gmail.com>
2022-04-18 17:17:06 +0000

    Update setup.sh with portainer agent

--- setup.sh
+++ setup.sh
@@ -33,6 +33,7 @@ echo -e "${green}$f Setupping Portainer...${reset}"
 
 docker volume create portainer_data
 
-docker run -d --name portainer -p 9000:9000 -p 8000:8000 --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+#docker run -d --name portainer -p 9000:9000 -p 8000:8000 --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker run -d -p 9001:9001 --name portainer_agent --restart=always -v \\.\pipe\docker_engine:\\.\pipe\docker_engine portainer/agent:2.6.3
 
 echo -e "${green}$f Finish...${reset}"
